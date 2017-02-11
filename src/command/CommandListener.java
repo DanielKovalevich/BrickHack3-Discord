94c121e1aead92bb6e7d8d0d1707122e791aa99d
VibrantClouds <bluecrecent0@gmail.com>
2017-02-11 14:34:47 -0500

    CommandNotFoundException

--- src/command/CommandListener.java
+++ src/command/CommandListener.java
@@ -1,5 +1,6 @@
 package command;
 
+import exceptions.CommandNotFoundException;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
--- src/exceptions/JsonContentNotFound.java
+++ src/exceptions/JsonContentNotFound.java
@@ -1,10 +1,8 @@
 package exceptions;
 
+@SuppressWarnings("serial")
 public class JsonContentNotFound extends Throwable{
 	
-
-	private static final long serialVersionUID = 1L;
-
 	public JsonContentNotFound() {
 		
 		System.out.println("[Warning] Failed to get content from API call.");
