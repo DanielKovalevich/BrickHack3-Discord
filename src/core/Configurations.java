a81394dac3d432875bd994391137c481e0d624fa
VibrantClouds <bluecrecent0@gmail.com>
2017-02-12 07:49:31 -0500

    Merge

--- src/core/Configurations.java
+++ src/core/Configurations.java
@@ -9,7 +9,7 @@ import java.util.Properties;
 
 public class Configurations {
 	private Properties configManager = null;
-	private String configFilePath = "C:\\Users\\Conrad\\Desktop";
+	private String configFilePath = "~/home";
 	
 	public Configurations() {
 		this.configManager = new Properties();
