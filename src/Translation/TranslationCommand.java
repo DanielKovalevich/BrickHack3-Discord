e85742acc9786f28a9d6d2198229f0cc8173e10a
VibrantClouds <bluecrecent0@gmail.com>
2017-02-11 19:41:55 -0500

    CONFIGURATION!?

--- src/Translation/TranslationCommand.java
+++ src/Translation/TranslationCommand.java
@@ -6,12 +6,13 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public abstract class TranslationCommand extends CommandABS {
 	
+	Configurations configManager = new Configurations();
+	
 	protected String to;
 	protected String from;
 	protected String text = "";
 	
-	core.Configurations configManager = new core.Configurations();
-	this.token = configManager.getPropertyValue("League_of_Legends_API_Key");
+	protected String token = configManager.getPropertyValue("League_of_Legends_API_Key");
 	
 	public TranslationCommand(String t, String f, String txt){
 		to = t;
